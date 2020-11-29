341a30912000f4565610cf505933bdfb8d6b5ef5
Wendi Yi <windyyih@outlook.com>
2020-11-29 17:52:34 +0800

    adding shiny ui and sentiment function

--- Desktop/NHH H20/BAN400/Term paper GIT/TP_BAN400.R
+++ Desktop/NHH H20/BAN400/Term paper GIT/TP_BAN400.R
@@ -6,6 +6,8 @@ library(tidyquant)
 library(tidyverse)
 library(dplyr)
 library(SentimentAnalysis)
+library(ggplot2)
+library(wordcloud)
 
 #reference
 #http://blueanalysis.com/iulianserban/Files/twitter_report.pdf
@@ -17,30 +19,80 @@ library(SentimentAnalysis)
 
 #input window
   #parameter : search key : ticker symbol or cashtag  or company name
+  #            average purchase price:  numbe
+  #            total shares :  number
+
   #            number of tweets: 0~18000
-  #            time period: last n days ?
+  #            time period: last n days ??
   #            dictionary: harvard or lm 
   #            uni_gram or bi_gram
+  
+# Define UI
+ui <- fluidPage(theme = shinytheme("lumen"),
+                titlePanel("Stock Trade Advice"),
+                
+                sidebarLayout(
+                  sidebarPanel(
+                    
+                    # enter search key
+                    textInput(inputId = "search_key", 
+                              label = strong("Search key"),
+                              value = "Enter ticker symbol"),
+                                # search key must be a ticker symbol
+                    
+                    #check if the user hold ths stock
+                    checkboxInput("hold", label = "Hold this stock", value = TRUE),
+                    
+                    #display only if the chcekbox"hold" is checked
+                    conditionalPanel(condition = "input.hold == true",
+                                     numericInput("shares", label = h3("Shares"), value = 100),
+                                     numericInput("price", label = h3("Average purchase price"), value = 100)),
+                    
+                    
+                    # select number of tweets to be retrived
+                    sliderInput("number_tweets", label = h3("number of tweets to be retrived"), 
+                                min = 500, max = 18000, value = 10000), 
+                                #min should be a reasonable value
+
+                    #select sentiment dictionary
+                    selectInput(inputId = "dictionary", label = h3("Sentiment dictionary"),
+                                choices = c("Harvard","LM"),
+                                selected = "Harvard"),
+                    
+                    #select sentiment method
+                    #selectInput(inputId = "method", label = h3("Sentiment method"),
+                     #           choices = c("Uni_gram","Bi_gram"),
+                    #            selected = "Uni_gram")
+                    
+                   
+                    #)
+                  #),
+                  
+                    # Output: Description, lineplot, and reference
+                  mainPanel(
+                    tabsetPanel(
+                      id = "output",
+                      tabPanel("twitter sentiment analysis", DT::dataTableOutput("mytable1")),
+                      tabPanel("10-k sentiment analysis", DT::dataTableOutput("mytable2")),
+                      tabPanel("trade advise", DT::dataTableOutput("mytable3"))
+                    ))
+  )))
 
 
 
-#search key must be included in this dataset
+#search key must be in this dataset
 ticker_symbol <- 
   read.csv("us_stock_code.csv")%>%
   select(code)
 
 
 #extract data
-  #search_tweets() an api token is needed!!
-  #get_mentions()
-
-
 tw_df<-
   search_tweets(
-  "$AAPL",             #=search key 
-  n = 1000,            #number of tweets
+  input$search_key,             #search key 
+  n =input$number_tweets,       #number of tweets
   type = "recent",
-  include_rts = FALSE, #exclude retweet
+  include_rts = FALSE,          #exclude retweet
   geocode = NULL,
   max_id = NULL,
   parse = TRUE,
@@ -48,11 +100,13 @@ tw_df<-
   retryonratelimit = FALSE,
   verbose = TRUE,
   lang = "en" )%>%
-  mutate(created_day = date(created),
-         created_time = time(created))
+  mutate(created_day = as.date(created),
+         created_time = as.time(created))%>%
+  select(-created)
+
 
 #preprocess function
-pre_df <- function(df){
+cleaning_tw_df <- function(df){
 df$text %>% 
   gsub("[[:punct:]]", " ", .) %>%        #remove punctuation
   gsub("[[:digit:]]", "", .) %>%         #remove digits
@@ -68,19 +122,93 @@ df$text %>%
 make_dtm <- function(df){
   df$text%>%
     DocumentTermMatrix(VCorpus(VectorSource()),
-                       control = list(stemming = TRUE )) #needed?
+                       control = list(stemming = TRUE )) #if needed
 }
 
 
-#sentiment analysis 
+#output-timelines - how many tweets retrived per day
 
-
-
-#timelines
 tw_df %>% 
   group_by(created_day, created_time)%>%
   mutate(tw_count = n())%>%
   ggplot(aes(x=created_day,y = created_time, fill = tw_count))+
   geom_point()
+
+  #or
+tw_df%>%
+  group_by(created_day)%>%
+  mutate(tw_count = n())%>%
+  ggplot(aes(x=created_day,y = tw_count))+
+  geom_bar()
+
+#output-wordcloud words appeared most frequently agmong tweets retrived
+
+
+
+
+#output-shortterm sentiment analysis base on twitter 
+  #sentiment analysis base on different dictionary
+  sentiment_analysis <- function(df){
+    if(input$dictionary == "Harvard"){
+    harvard_sentiment(df)
+  }else{
+    lm_sentiment(df)
+    }
+  }
+
+  # harvard sentiment function
+  harvard_sentiment <- funtion(df){
+    if(unique(df$created_day) > 1){
+      df %>%
+        mutate(harvard_score = analyzeSentiment(text)$SentimentGI)%>%
+        groupby(created_day)%>%
+        summarize( daily_harvard_score = mean (harvard_score))%>%
+        ggplot(aes(x= created_day, y= daily_harvard_score))+
+        geom_point(show.legend = F, col = "red")+
+        geom_line()+
+        scale_x_continuous(name="Date", limits=c(min(created_day)+1, max(created_day)))
+        geom_smooth(alpha = 0, method = "lm")
+    }else{
+      df %>%
+        mutate(harvard_score = analyzeSentiment(text)$SentimentGI)%>%
+        groupby(created_day)%>%
+        summarize( daily_harvard_score = mean (harvard_score))%>%
+        ggplot(aes(x= created_day, y= daily_harvard_score))+
+        geom_point(show.legend = F, col = "red")+
+        geom_line()+
+        geom_smooth(alpha = 0, method = "lm")
+    }
+  }
+  
+  # LM sentiment function
+  lm_sentiment <-  funtion(df){
+    if(unique(df$created_day) > 1){
+      df %>%
+        mutate(lm_score = analyzeSentiment(text)$SentimentLM)%>%
+        groupby(created_day)%>%
+        summarize( daily_lm_score = mean (lm_score))%>%
+        ggplot(aes(x= created_day, y= daily_lm_score))+
+        geom_point(show.legend = F, col = "red")+
+        geom_line()+
+        scale_x_continuous(name="Date", limits=c(min(created_day)+1, max(created_day)))
+        geom_smooth(alpha = 0, method = "lm")
+    }else{
+      df %>%
+        mutate(lm_score = analyzeSentiment(text)$SentimentLM)%>%
+        groupby(created_day)%>%
+        summarize( daily_lm_score = mean (lm_score))%>%
+        ggplot(aes(x= created_day, y= daily_lm_score))+
+        geom_point(show.legend = F, col = "red")+
+        geom_line()++
+        geom_smooth(alpha = 0, method = "lm")
+    }
+  }
+  
+
+#output-longterm sentiment analysis base on 10-k
+
+
+
+
 
 
